56553446e3e46cbc471e79f5aea508926187e4a1
Christian Schmeichel <cschmeichel@gmail.com>
2017-05-23 13:39:11 -0300

    forgot date range

--- app/models/premium_subscription.rb
+++ app/models/premium_subscription.rb
@@ -6,12 +6,16 @@ class PremiumSubscription < ApplicationRecord
 
   def self.query_results(params)
     max_entries = params[:max_entries].to_i if params[:max_entries].present?
-    query_date  = DateTime.new(params[:date][:year].to_i, params[:date][:month].to_i, params[:date][:day].to_i) if
-        params[:date] && params[:date][:year].present? && params[:date][:month].present? && params[:date][:day].present?
+    start_date  = DateTime.new(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i) if
+        params[:start_date] && params[:start_date] && params[:start_date][:year].present? && params[:start_date][:month].present? && params[:start_date][:day].present?
+    end_date    = DateTime.new(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i) if
+        params[:end_date] && params[:end_date] && params[:end_date][:year].present? && params[:end_date][:month].present? && params[:end_date][:day].present?
+    end_date ||= start_date # only end date assignment is not allowed
+
     sorting     = params[:sort_by].present? ? params[:sort_by] : 'created_at'
 
     query = PremiumSubscription.order("#{sorting} DESC")
-    query = query.where('created_at BETWEEN ? AND ?', query_date.beginning_of_day, query_date.end_of_day) if query_date
+    query = query.where('created_at BETWEEN ? AND ?', start_date.beginning_of_day, end_date.end_of_day) if start_date
     query = query.limit(max_entries) if max_entries
 
     return query
